06636dcc2546bc918fc405d9f92891fddcf61034
Minnggg <phanngocminh168@gmail.com>
2023-02-17 16:36:20 +0700

    fix 6024

--- DSA06024.cpp
+++ DSA06024.cpp
@@ -1,43 +1,33 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int a[1005];
-
-int check(int n)
-{
-    for (int i=0; i<n; i++)
-    {
-        if (a[i] > a[i+1])
-            return 1;
-    }
-    return 0;
-}
 
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i=0; i<n; i++)
-        cin >> a[i];
-    for (int i=0; i<n-1; i++)
-    {
-        int m = a[i], idx = i;
-        for (int j = i+1; j<n; j++)
-        {
-            if (a[j]<m)
-            {
-                m = a[j];
-                idx = j;
-            }
-        }
-        cout << "Buoc " << i+1 << ": ";
-        a[idx] = a[i];
-        a[i] = m;
-        for (int i=0; i<n; i++)
-            cout << a[i] << ' ';
-        cout << endl;
-        if (check(n)==0)
-            break;
-    }
+ 	int n;
+ 	cin >> n;
+ 	int a[n];
+ 	for(int i=0;i<n;i++) cin >> a[i];
+ 	for(int i=0;i<n-1;i++)
+ 	{
+ 		int minn=a[i];
+ 		int idx=i;
+ 		for(int j=i+1;j<n;j++)
+ 		{
+ 			if(a[j]<minn)
+ 			{
+ 				minn=a[j];
+				idx=j;	
+			}
+		}
+		int check=1;
+		for(int j=0;j<n-1;j++)
+		{
+			if(a[j]>a[j+1]) check=0;
+		}
+		cout<< "Buoc "<<i+1<<": ";
+		swap(a[i],a[idx]);
+		for(int j=0;j<n;j++) cout<<a[j]<<" ";
+		cout << endl;
+		if(check==1) break;
+	}
 }
--- DSA06026.cpp
+++ DSA06026.cpp
@@ -1,34 +1,37 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int a[1005];
-
-int check(int n)
-{
-    for (int i=0; i<n-1; i++)
-    {
-        if (a[i] > a[i+1])
-            return 1;
-    }
-    return 0;
-}
 
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i=0; i<n; i++)
-        cin >> a[i];
-    int i=0;
-    while (check(n))
+ 	int n;
+ 	cin >> n;
+ 	int a[n];
+ 	for(int i=0;i<n;i++) cin >> a[i];
+ 	int cnt=1;
+ 	// hàm sắp xếp nổi bọt (bubble sort)
+    for(int i=0;i<n-1;i++)
     {
-        for (int i=0; i<n-1; i++)
-            if (a[i] > a[i+1])
-                swap(a[i], a[i+1]);
-        cout << "Buoc " << ++i << ": ";
-        for (int j=0; j<n; j++)
-            cout << a[j] << ' ';
-        cout << endl;
-    }
+    	
+    	for(int j=0;j<n-i-1;j++)
+    	{
+    		if(a[j]>a[j+1]) swap(a[j],a[j+1]);
+		}
+		int check=1;
+		for(int j=0;j<n-1;j++) 
+		{
+			for(int k=j+1;k<n;k++) 
+			{
+				if(a[j]>a[k]) check=0;			}
+		}
+		cout<<"Buoc "<< i+1<<": ";
+		for(int j=0;j<n;j++) cout << a[j] <<" ";
+		cout <<endl;
+		
+		if(check==1)
+		{
+			break;
+		}
+		
+		
+	}
 }
